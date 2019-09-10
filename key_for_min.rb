d56abc53eb95f2bdba464de5b1563b83b5c4a93c
Laura Berge <lauraeberge@gmail.com>
2019-09-10 00:32:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   key_with_min_value = nil
   name_hash.each do |name, num|
     if num < i || i == 0
-      num = i
+      i = num
       key_with_min_value = name
     end
   end
